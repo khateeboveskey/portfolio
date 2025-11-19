03e8a717fa8ba42bb84b327f16b82e2d8c930c06
A.Rahman Al-Khateeb <khateeboveskey@gmail.com>
2025-11-20 00:37:01 +0300

    refactor: update meta tags and restructure error page component

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -9,13 +9,24 @@
 <script setup lang="ts">
 useHead({
   meta: [
-    { name: 'author', content: 'Abdulrahman Al-Khateeb' },
-    { name: 'description', content: 'Crafting digital experiences with passion and precision.' },
-    { name: 'keywords', content: 'web development, software engineering, portfolio, projects, Abdulrahman Al-Khateeb' }
+    { name: "author", content: "Abdulrahman Al-Khateeb" },
+    {
+      name: "description",
+      content: "Crafting digital experiences with passion and precision.",
+    },
+    {
+      name: "keywords",
+      content:
+        "web development, software engineering, portfolio, projects, Abdulrahman Al-Khateeb",
+    },
   ],
-  titleTemplate: '%s | Khateeb',
+  titleTemplate: "%s | Khateeb",
   link: [
-    { rel: 'stylesheet', type: 'text/css', href: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css' },
-  ]
+    {
+      rel: "stylesheet",
+      type: "text/css",
+      href: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css",
+    },
+  ],
 });
 </script>
