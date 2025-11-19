01643b49637f287dd876d08924bf89bd09bede2e
A.Rahman Al-Khateeb <khateeboveskey@gmail.com>
2025-11-20 00:18:09 +0300

    refactor: replace all project with Nuxt 4 cleaner structure

--- app.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <NuxtLayout class="font-sans bg-accent-background overflow-x-hidden">
-    <NuxtPage />
-  </NuxtLayout>
-</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,32 +1,12 @@
-import tailwindcss from "@tailwindcss/vite";
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  css: ['assets/css/main.css'],
-  content: {
-    build: {
-      markdown: {
-        highlight: {
-          theme: 'one-dark-pro',
-        }
-      }
-    },
-  },
-  vite: {
-    plugins: [
-      tailwindcss(),
-    ],
-  },
-  lucide: {
-    namePrefix: 'Icon'
-  },
+
   modules: [
-    'nuxt-svgo',
-    '@nuxt/content',
+    '@nuxt/eslint',
+    '@nuxt/hints',
     '@nuxt/image',
-    'nuxt-lucide-icons',
-    'nuxt-marquee',
-  ],
+    '@nuxt/ui'
+  ]
 })
