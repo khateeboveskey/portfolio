b1f5b70c3535e0e062884f2a15a92a04a204604b
A.Rahman Al-Khateeb <khateeboveskey@gmail.com>
2026-01-22 20:04:38 +0300

    feat: update color configuration in app.config.ts and main.css for improved theming

--- app/app.config.ts
+++ app/app.config.ts
@@ -2,7 +2,7 @@ export default defineAppConfig({
   ui: {
     colors: {
       primary: 'brand',
-      neutral: 'neutral',
+      neutral: 'foreground',
     },
     button: {
       defaultVariants: {
